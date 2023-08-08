294588bc8c91aac25085496eb2c51cbf3679742b
Ali Sabet <asabet@uwaterloo.ca>
2023-08-08 00:13:55 +0000

    First run works!

---
+++ blora.py
@@ -0,0 +1,200 @@
+import torch
+from peft.utils.other import transpose
+from torch.nn import functional as F
+from peft.tuners.lora import Linear
+
+
+import math
+from typing import List, Optional, Tuple, Union
+
+import torch
+import torch.nn.functional as F
+import torch.utils.checkpoint
+from torch import nn
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+
+
+from transformers.activations import ACT2FN
+from transformers.modeling_outputs import (
+    BaseModelOutputWithPast,
+    CausalLMOutputWithPast,
+    SequenceClassifierOutputWithPast,
+)
+from transformers.modeling_utils import PreTrainedModel
+from transformers.utils import (
+    add_start_docstrings,
+    add_start_docstrings_to_model_forward,
+    logging,
+    replace_return_docstrings,
+)
+from transformers.models.llama.configuration_llama import LlamaConfig
+
+from transformers.models.llama.modeling_llama import (
+    LLAMA_INPUTS_DOCSTRING,
+    LLAMA_START_DOCSTRING,
+    _CONFIG_FOR_DOC,
+)
+
+
+def forward(self, x: torch.Tensor):
+    previous_dtype = x.dtype
+    if self.active_adapter not in self.lora_A.keys():
+        return F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
+    if self.disable_adapters:
+        if self.r[self.active_adapter] > 0 and self.merged:
+            self.unmerge()
+        result = F.linear(
+            x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias
+        )
+    elif self.r[self.active_adapter] > 0 and not self.merged:
+        result = F.linear(
+            x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias
+        )
+
+        x = x.to(self.lora_A[self.active_adapter].weight.dtype)
+
+        batch = list(zip(x, self.batch_lora_ids))
+
+        # x0, x1 = x[0], x[1]
+        # result0, result1 = result[0], result[1]
+
+        # lora_out = torch.stack(
+        #     [
+        #         self.scaling
+        #         * self.lora_B[lora_id](
+        #             self.lora_A[lora_id](self.lora_dropout[lora_id](x))
+        #         )
+        #         if lora_id in self.lora_A.keys()
+        #         else torch.zeros_like(result[0])
+        #         for x, lora_id in batch
+        #     ]
+        # )
+
+        # rewrite as for loop
+        lora_out = torch.zeros_like(result)
+        for i, (x, lora_id) in enumerate(batch):
+            if lora_id in self.lora_A.keys():
+                lora_out[i] = self.scaling[lora_id] * self.lora_B[lora_id](
+                    self.lora_A[lora_id](self.lora_dropout[lora_id](x))
+                )
+
+        result += lora_out
+
+    else:
+        result = F.linear(
+            x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias
+        )
+
+    result = result.to(previous_dtype)
+
+    return result
+
+
+@add_start_docstrings_to_model_forward(LLAMA_INPUTS_DOCSTRING)
+@replace_return_docstrings(
+    output_type=CausalLMOutputWithPast, config_class=_CONFIG_FOR_DOC
+)
+def LlamaForCausalLM_forward(
+    self,
+    input_ids: torch.LongTensor = None,
+    attention_mask: Optional[torch.Tensor] = None,
+    position_ids: Optional[torch.LongTensor] = None,
+    past_key_values: Optional[List[torch.FloatTensor]] = None,
+    inputs_embeds: Optional[torch.FloatTensor] = None,
+    labels: Optional[torch.LongTensor] = None,
+    use_cache: Optional[bool] = None,
+    output_attentions: Optional[bool] = None,
+    output_hidden_states: Optional[bool] = None,
+    return_dict: Optional[bool] = None,
+) -> Union[Tuple, CausalLMOutputWithPast]:
+    r"""
+    Args:
+        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+            Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
+            config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
+            (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+
+    Returns:
+
+    Example:
+
+    ```python
+    >>> from transformers import AutoTokenizer, LlamaForCausalLM
+
+    >>> model = LlamaForCausalLM.from_pretrained(PATH_TO_CONVERTED_WEIGHTS)
+    >>> tokenizer = AutoTokenizer.from_pretrained(PATH_TO_CONVERTED_TOKENIZER)
+
+    >>> prompt = "Hey, are you conscious? Can you talk to me?"
+    >>> inputs = tokenizer(prompt, return_tensors="pt")
+
+    >>> # Generate
+    >>> generate_ids = model.generate(inputs.input_ids, max_length=30)
+    >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
+    ```"""
+
+    output_attentions = (
+        output_attentions
+        if output_attentions is not None
+        else self.config.output_attentions
+    )
+    output_hidden_states = (
+        output_hidden_states
+        if output_hidden_states is not None
+        else self.config.output_hidden_states
+    )
+    return_dict = (
+        return_dict if return_dict is not None else self.config.use_return_dict
+    )
+
+    # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+    outputs = self.model(
+        input_ids=input_ids,
+        attention_mask=attention_mask,
+        position_ids=position_ids,
+        past_key_values=past_key_values,
+        inputs_embeds=inputs_embeds,
+        use_cache=use_cache,
+        output_attentions=output_attentions,
+        output_hidden_states=output_hidden_states,
+        return_dict=return_dict,
+    )
+
+    hidden_states = outputs[0]
+    if self.pretraining_tp > 1:
+        lm_head_slices = self.lm_head.weight.split(
+            self.vocab_size // self.pretraining_tp, dim=0
+        )
+        logits = [
+            F.linear(hidden_states, lm_head_slices[i])
+            for i in range(self.pretraining_tp)
+        ]
+        logits = torch.cat(logits, dim=-1)
+    else:
+        logits = self.lm_head(hidden_states)
+    logits = logits.float()
+
+    loss = None
+    if labels is not None:
+        # Shift so that tokens < n predict n
+        shift_logits = logits[..., :-1, :].contiguous()
+        shift_labels = labels[..., 1:].contiguous()
+        # Flatten the tokens
+        loss_fct = CrossEntropyLoss()
+        shift_logits = shift_logits.view(-1, self.config.vocab_size)
+        shift_labels = shift_labels.view(-1)
+        # Enable model parallelism
+        shift_labels = shift_labels.to(shift_logits.device)
+        loss = loss_fct(shift_logits, shift_labels)
+
+    if not return_dict:
+        output = (logits,) + outputs[1:]
+        return (loss,) + output if loss is not None else output
+
+    return CausalLMOutputWithPast(
+        loss=loss,
+        logits=logits,
+        past_key_values=outputs.past_key_values,
+        hidden_states=outputs.hidden_states,
+        attentions=outputs.attentions,
+    )
